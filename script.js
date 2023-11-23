7f1a417f3c74026a148818b54b65faab74bf609f
jjcazau <jjcazau@users.noreply.github.com>
2023-11-23 14:13:38 +1100

    Change number of crosses.

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var teams = document.querySelectorAll('.crosses');
+    initializeGame(document.getElementById('numCrosses').value);
+    // Help modal
+    var modal = document.getElementById("rulesModal");
+    var btn = document.getElementById("helpBtn");
+    var span = document.getElementsByClassName("close")[0];
 
+        // Event listener for the change button
+        document.getElementById('changeCrosses').addEventListener('click', function() {
+            initializeGame(document.getElementById('numCrosses').value);
+        });
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+// Reset button functionality if needed
+document.getElementById("resetBtn").addEventListener('click', function() {
+    initializeGame(document.getElementById('numCrosses').value);
+});
+});
+
+function initializeGame(crossCount) {
+    var teams = document.querySelectorAll('.crosses');
     teams.forEach(function(team) {
-        for (var i = 0; i < 10; i++) {
+        team.innerHTML = ''; // Clear existing crosses
+        for (var i = 0; i < crossCount; i++) {
             var cross = document.createElement('div');
             cross.classList.add('cross');
             cross.textContent = 'X';
@@ -12,4 +44,4 @@ document.addEventListener('DOMContentLoaded', function() {
             team.appendChild(cross);
         }
     });
-});
+}
