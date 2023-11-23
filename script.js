2cda537bc6c04db97f663cf93349372595909895
jjcazau <jjcazau@users.noreply.github.com>
2023-11-23 15:01:22 +1100

    better interactivity

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     initializeGame(document.getElementById('numCrosses').value);
-    // Help modal
+    // Help modal setup
     var modal = document.getElementById("rulesModal");
     var btn = document.getElementById("helpBtn");
     var span = document.getElementsByClassName("close")[0];
 
-        // Event listener for the change button
-        document.getElementById('changeCrosses').addEventListener('click', function() {
-            initializeGame(document.getElementById('numCrosses').value);
-        });
+    // Increase and decrease button event listeners
+    document.getElementById('increaseCrosses').addEventListener('click', function() {
+        changeNumberOfCrosses(1);
+    });
 
+    document.getElementById('decreaseCrosses').addEventListener('click', function() {
+        changeNumberOfCrosses(-1);
+    });
+
+    // Event listener for applying the number of crosses directly after change
+    // This can be omitted if you wish to use an 'apply' button instead
+    function changeNumberOfCrosses(change) {
+        var numCrossesInput = document.getElementById('numCrosses');
+        var currentVal = parseInt(numCrossesInput.value);
+        var newVal = currentVal + change;
+
+        // Check the range of the new value
+        if(newVal >= 1 && newVal <= 20) {
+            numCrossesInput.value = newVal;
+            initializeGame(newVal); // Re-initialize the game with the new value
+        }
+    }
+
+    // Help modal interactions
     btn.onclick = function() {
         modal.style.display = "block";
     }
@@ -24,15 +43,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-// Reset button functionality if needed
-document.getElementById("resetBtn").addEventListener('click', function() {
-    initializeGame(document.getElementById('numCrosses').value);
-});
+    // Reset button functionality
+    document.getElementById("resetBtn").addEventListener('click', function() {
+        initializeGame(document.getElementById('numCrosses').value);
+    });
 });
 
 function initializeGame(crossCount) {
     var teams = document.querySelectorAll('.crosses');
     teams.forEach(function(team) {
+        // Reset X font style
+        team.style.color = 'black';
+        team.style.fontSize = '1em';
+        
         team.innerHTML = ''; // Clear existing crosses
         for (var i = 0; i < crossCount; i++) {
             var cross = document.createElement('div');
@@ -40,8 +63,20 @@ function initializeGame(crossCount) {
             cross.textContent = 'X';
             cross.onclick = function() {
                 this.remove();
+                // Check if the team is out
+                if (team.getElementsByClassName('cross').length === 0) {
+                    team.textContent = 'Out!';
+                    team.style.color = 'red';
+                    team.style.fontSize = '3em';
+                }
             };
             team.appendChild(cross);
+        }
+        // Initial check if the team is out
+        if (team.getElementsByClassName('cross').length === 0) {
+            team.textContent = 'Out!';
+            team.style.color = 'red';
+            team.style.fontSize = '3em';
         }
     });
 }
